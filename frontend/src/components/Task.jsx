c7ae3912454c85dc8b069813fc171046f236d4e7
Joshua Brunscheen <114787256+josh-brunscheen@users.noreply.github.com>
2025-10-02 22:20:13 -0400

    json schema

---
+++ frontend/src/components/Task.jsx
@@ -0,0 +1,14 @@
+// JSON schema for a give task
+// Task name - string value
+// Task due date - probably just a string value again or some sort of date type
+// Task notes - string value
+
+function Task({taskData}) {
+  return (
+    <div className="text-6xl font-bold text-[#5A311F]">
+        PRODUCKTIVITY
+    </div>
+  )
+}
+
+export default Task
